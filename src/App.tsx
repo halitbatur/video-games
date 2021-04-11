e97352f7bb7efdc1ef1db4433dba42e41941517a
halit <halitfuatbatur@gmail.com>
2021-04-11 15:53:08 +0300

    use map for routes and links to make it easy to add more routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,39 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import VideoGamesPage from "../src/containers/videoGamesPage/VideoGamesPage";
 import Navbar from "../src/components/navbar/Navbar";
 
+interface Route {
+  path: string;
+  linkName: string;
+  shadow: string;
+  Component: () => JSX.Element;
+}
+
+export const ROUTES: Route[] = [
+  {
+    path: "/",
+    linkName: "VIDEO GAMES",
+    shadow: "VIDEO",
+    Component: VideoGamesPage,
+  },
+  {
+    path: "/contact",
+    linkName: "CONTACT",
+    shadow: "CONTACT",
+    Component: VideoGamesPage,
+  },
+];
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path="/">
-            <VideoGamesPage />
-          </Route>
-          <Route path="/contact">Contact Page</Route>
+          {ROUTES.map(({ path, Component }) => (
+            <Route exact path={path} key={path}>
+              <Component />
+            </Route>
+          ))}
         </Switch>
       </div>
     </Router>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { NavbarContainer, Shadow, LinkContainer } from "./NavbarStyled";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { ROUTES } from "../../App";
 
 const Navbar = () => {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <NavbarContainer>
       <nav>
-        <LinkContainer>
-          <Shadow>VIDEO</Shadow>
-          <Link to="/">VIDEO GAMES</Link>
-        </LinkContainer>
-        <LinkContainer>
-          <Shadow>CONTACT</Shadow>
-          <Link to="/contact">CONTACT</Link>
-        </LinkContainer>
+        {ROUTES.map((route) => (
+          <LinkContainer key={route.path}>
+            {location.pathname === route.path && (
+              <Shadow>{route.shadow}</Shadow>
+            )}
+            <Link to={route.path}>{route.linkName}</Link>
+          </LinkContainer>
+        ))}
       </nav>
     </NavbarContainer>
   );
